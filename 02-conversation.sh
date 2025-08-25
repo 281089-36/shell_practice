89ba5bc3a2ec470e52be9ac510a517cbee3d9e1f
msivakumarreddy <info@joindevops.com>
2025-08-25 17:51:06 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Trump: Hey musk, How are you?"
+echo "Musk:: Hello Trump, I am fine. How are you doing"
+echo "Trump: I am fine too. What's up?"
+echo "musk:: Nothing, just going to Mars now, will you come"
+echo "Trump:: Sorry, You carry on! I will come once you come back"
+ 
