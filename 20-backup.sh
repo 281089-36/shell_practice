ed7da6429a7143f942d9f1756b62fb4a1b8c02ce
msivakumarreddy <info@joindevops.com>
2025-09-18 14:11:13 +0530

    hello world started

--- 20-backup.sh
+++ 20-backup.sh
@@ -84,44 +84,4 @@ then
     fi
 else
     echo -e "No log files found older than 14 days ... $Y SKIPPING $N"
-fi/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-mkdir -p $LOGS_FOLDER
-
-
-
-USAGE(){
-    echo -e "$R USAGE:: $N sh 20-backup.sh <source-dir> <destination-dir> <days(optional)>"
-}
-
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
-
-if [$# -lt 2 ]
-then
-    USAGE
 fi
-
-check_root(){
-        if [ $USERID -ne 0 ]
-    then
-        echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-        exit 1 #give other than 0 upto 127
-    else
-        echo "You are running with root access" | tee -a $LOG_FILE
-    fi
-}
